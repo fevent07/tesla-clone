ee9481887d0d686e93df0191e3fc4b4a5cbf3733
fevent07 <feventsige.gg@gmail.com>
2022-08-09 08:23:06 -0700

    test1

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,6 +22,7 @@ function Header() {
           <img src="/images/logo.svg" alt=" logo"/>
         </a>
         <Menu>
+          <></>
               {cars && cars.map((car, index)=>(
                 <p><a href ="#">{car}</a></p>
 
